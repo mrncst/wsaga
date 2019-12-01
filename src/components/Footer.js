123328b9fdeae1da262e1e05bd588595949e1d5c
mrncst <cassstilho@gmail.com>
2019-12-01 18:27:18 -0300

    Footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return(
         <div className='footer'>
-            <p><a href='https://twitter.com/wsaga_cc' target="_blank" rel="noopener">Siga a gente no Twitter!</a></p>
+            <p><a href='https://twitter.com/wsagaoficial' target="_blank" rel="noopener">Siga a gente no Twitter!</a></p>
         </div>
     );
 };
